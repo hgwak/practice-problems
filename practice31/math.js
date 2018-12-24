a8d3e2c1f4c26a9d42eec295d8676c1069b84bb5
hgwak <hannelgwak@gmail.com>
2018-12-24 14:10:58 -0800

    practice31 complete'

---
+++ practice31/math.js
@@ -0,0 +1,7 @@
+function cocococombomath(num1,num2,num3,num4,num5){
+    var multResult = num1 * num2;
+    var sumResult = num3 + multResult;
+    var divideResult = sumResult / num4;
+    var subtractResult = divideResult - num5;
+    return console.log(subtractResult);
+};
