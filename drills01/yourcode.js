33cada708156f6919964a7dbb115cb2a060cbab1
hgwak <hannelgwak@gmail.com>
2019-01-07 10:13:22 -0800

    working

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,11 +1,23 @@
 
 
 function sumArray(a){
-
+    var sum = 0;
+    for (var i =0; i<a.length;i++){
+        sum+=a[i];
+    }
+    return sum;
 }
 
 function fitWithinVal(array, val){
-
+ var newArray=[];
+ var newArraySum=0;
+ for (var i=0;i<array.length;i++){
+    newArraySum+=array[i];
+     if(newArraySum>=val){
+        array.splice(i);
+     }
+ }
+ return array;
 }
 
 function getAllNamesShorterThan(a, len){
