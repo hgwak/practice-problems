d8e74315a5a0d8fec5ea9aa1fbd8c661f1476de1
Sky Gwak <hannelgwak@gmail.com>
2019-07-16 18:14:54 -0700

    starting again

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,10 +1,30 @@
 
-function sumArray(){
-	
+function sumArray(array){
+    let sumOfArray = 0;
+	for(let i = 0; i < array.length; i++){
+        sumOfArray += array[i]
+    }
+    return sumOfArray;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(array, someNumber){
+    let sumOfArray = 0;
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if(array[i]>someNumber){
+            continue;
+        }
+        sumOfArray += array[i]
+        if(sumOfArray > someNumber){
+            sumOfArray -= array[i];
+            continue;
+        }
+        newArray.push(array[i])
+        if(sumOfArray === someNumber){
+            break;
+        }
+    }
+    return newArray;
 }
 
 function getAllNamesShorterThan(){
