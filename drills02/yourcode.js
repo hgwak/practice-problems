14e19d18414dff8075673a03bc9f4630cc82ccc1
hgwak <hannelgwak@gmail.com>
2019-07-22 17:31:39 -0700

    almost done with drills2

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,15 +1,42 @@
 
 
-function countOccurences(){
-
+function countOccurences(arrayOfWords, word){
+    let count = 0;
+    arrayOfWords.forEach((someWord)=>{
+        if(someWord === word){
+            count++;
+        }
+    })
+    return count;
 }
 
-function wordLengths(){
-
+function wordLengths(arrayOfWords){
+    let newArray = arrayOfWords.map((word)=>{
+        return word.length
+    })
+    return newArray;
 }
 
-function getMinMaxMean(){
-
+function getMinMaxMean(array){
+    let minMaxMean = {
+        min:0,
+        max:0,
+        mean:0
+    };
+    array.forEach((number, index)=>{
+        if (!index) {
+            minMaxMean.min = number;
+        }
+        if(number < minMaxMean.min){
+            minMaxMean.min = number;
+        }
+        if(number > minMaxMean.max){
+            minMaxMean.max = number;
+        }
+        minMaxMean.mean += number;
+    })
+    minMaxMean.mean /= array.length;
+    return minMaxMean;
 }
 
 function findMode(){
