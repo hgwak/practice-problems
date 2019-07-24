026ff7f7f44175af007077b53a72adfd44e68585
hgwak <hannelgwak@gmail.com>
2019-07-24 16:21:13 -0700

    completed drills 04

--- drills04/yourcode.js
+++ drills04/yourcode.js
@@ -1,17 +1,58 @@
 
 
-function changeElements(){
-
+function changeElements(htmlClass){
+    let newElement = $(htmlClass);
+    for(let i = 0; i < newElement.length; i++){
+        let wordNumber = newElement[i].innerHTML;
+        switch(wordNumber){
+            case "one":
+                newElement[i].innerHTML = 1;
+                break;
+            case "two":
+                newElement[i].innerHTML = 2;
+                break;
+            case "three":
+                newElement[i].innerHTML = 3;
+                break;
+            case "four":
+                newElement[i].innerHTML = 4;
+                break;
+            case "five":
+                newElement[i].innerHTML = 5;
+                break;
+            case "six":
+                newElement[i].innerHTML = 6;
+                break;
+            case "seven":
+                newElement[i].innerHTML = 7;
+                break;
+            case "eight":
+                newElement[i].innerHTML = 8;
+                break;
+            case "nine":
+                newElement[i].innerHTML = 9;
+                break;
+            default:
+                break;
+        }
+    }  
 }
 
-function appendTextToElement(){
-
+function appendTextToElement(htmlClass, someWord){
+    let objectOfElements = $(htmlClass);
+    for(let i = 0; i < objectOfElements.length; i++){
+        objectOfElements[i].innerHTML += someWord;
+    }
 }
 
-function addClass(){
+
+function addClass(htmlClass, someWord){
+    let arrayOfHTML = $(htmlClass);
+    $(htmlClass).addClass(someWord);
+    return arrayOfHTML.length;
 }
 
-function removeElements(){
-
+function removeElements(htmlClass){
+    $(htmlClass).remove();
 }
 
