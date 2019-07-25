bd519531ee615311a13c577f0f9f03fef41d0f66
hgwak <hannelgwak@gmail.com>
2019-07-24 19:41:12 -0700

    drills 6 done

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,23 +1,38 @@
 
 
-function removeClassFromElement(){
+function removeClassFromElement(someClass, someOtherClass){
+    $(someClass).removeClass(someOtherClass)
+}
+
+function toggleClassOnElement(someElement,addClass){
+	$(someElement).toggleClass(addClass)
+}
+
+function hideElements(someElement, hideClass ){
+    if(hideClass === 'hide'){
+        $(someElement).hide();
+    }
+    if(hideClass==='removeChildren'){
+        $(`${someElement} > *`).remove();
+    }
+    if(hideClass==="removeSelf"){
+        $(someElement).remove();
+    }
 	
 }
 
-function toggleClassOnElement(){
-	
+function addAttributeToElement(first, second, third ){
+    $(first).attr(second, third)
 }
 
-function hideElements(  ){
-	
-}
-
-function addAttributeToElement( ){
-	
-}
-
-function putPosInElement(  ){
-	
+function putPosInElement(someClass){
+    let coordinates = $(someClass).position();
+    let newCoordinates = {
+        x:coordinates.top,
+        y:coordinates.left
+    }
+    $(someClass).html("x: "+newCoordinates.x + 'px' + "<br>" + "y: "+newCoordinates.y + 'px');
+    return newCoordinates;
 }
 
 
